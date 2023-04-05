b26cae2551ac95a8b801b01671ade0ae79368e93
maewakka <chlguddn7@gmail.com>
2023-04-05 14:35:30 +0900

    [Silver II] Title: 스택 수열, Time: 236 ms, Memory: 135084 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,34 +1,39 @@
 import sys
 from collections import deque
 
-
 def solution():
     n = int(sys.stdin.readline())
-
+    inputs = []
     stack = []
-    result = []
-    index = 1
+    answer = []
+    nums = deque([i for i in range(1, n+1)])
+    isPos = True
 
     for _ in range(n):
-        this_num = int(sys.stdin.readline())
+        inputs.append(int(sys.stdin.readline()))
 
-        if this_num >= index:
-            for _ in range(this_num-index+1):
-                stack.append(index)
-                index += 1
-                result += '+'
+    index = 0
+    while True:
+        if not stack and index == n:
+            break
 
-        if this_num < index:
-            while stack and this_num == stack[-1]:
-                stack.pop()
-                result += '-'
+        if not stack or stack[-1] != inputs[index]:
+            if not nums:
+                isPos = False
+                break
+            else:
+                stack.append(nums.popleft())
+                answer.append('+')
+        else:
+            stack.pop()
+            index += 1
+            answer.append('-')
 
-    if not stack:
-        for cal in result:
-            print(cal)
+    if isPos:
+        for a in answer:
+            print(a)
     else:
         print("NO")
-
 
 
 if __name__ == "__main__":
