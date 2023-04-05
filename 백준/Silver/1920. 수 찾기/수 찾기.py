32181c55e816655f7162bd81b37669d6f53676a9
maewakka <chlguddn7@gmail.com>
2023-04-05 15:43:13 +0900

    [Silver IV] Title: 수 찾기, Time: 236 ms, Memory: 149984 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,43 +1,21 @@
-import heapq
 import sys
-from collections import deque
-from queue import PriorityQueue
-import heapq
-sys.setrecursionlimit(10 ** 7)
 
 def solution():
-    N = int(sys.stdin.readline().strip())
-    array = list(map(int, sys.stdin.readline().strip().split(" ")))
-    M = int(sys.stdin.readline().strip())
-    find_list = list(map(int, sys.stdin.readline().strip().split(" ")))
+    n = int(sys.stdin.readline())
+    inputs = list(map(int, sys.stdin.readline().split()))
+    nums = {}
 
-    array.sort()
+    for i in inputs:
+        nums[i] = True
 
-    for i in find_list:
-        start = 0
-        end = len(array)-1
+    m = int(sys.stdin.readline())
+    inputs = list(map(int, sys.stdin.readline().split()))
 
-        while True:
-            middle = (start + end) // 2
-
-            if i == array[middle]:
-                print(1)
-                break
-
-            if start > middle:
-                print(0)
-                break
-
-            if array[middle] >= i:
-                end = middle-1
-            else:
-                start = middle+1
-
-
-
-
-
-
+    for i in inputs:
+        if nums.get(i) != None:
+            print(1)
+        else:
+            print(0)
 
 
 if __name__ == "__main__":
