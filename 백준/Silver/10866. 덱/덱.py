e1ee34b6025d3aad56ea0beea371d6bb37ab3ca9
maewakka <chlguddn7@gmail.com>
2023-04-06 15:08:19 +0900

    [Silver IV] Title: 덱, Time: 176 ms, Memory: 116920 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,56 @@
+import sys
+from collections import deque
+
+def solution():
+    n = int(sys.stdin.readline())
+
+    queue = deque()
+    size = 0
+    answer = []
+
+    for _ in range(n):
+        orders = sys.stdin.readline().split()
+        order = orders[0]
+
+        if order == 'push_front':
+            queue.appendleft(int(orders[1]))
+            size += 1
+        elif order == 'push_back':
+            queue.append(int(orders[1]))
+            size += 1
+        elif order == 'pop_front':
+            if queue:
+                answer.append(queue.popleft())
+                size -= 1
+            else:
+                answer.append(-1)
+        elif order == 'pop_back':
+            if queue:
+                answer.append(queue.pop())
+                size -= 1
+            else:
+                answer.append(-1)
+        elif order == 'size':
+            answer.append(size)
+        elif order == 'empty':
+            if queue:
+                answer.append(0)
+            else:
+                answer.append(1)
+        elif order == 'front':
+            if queue:
+                answer.append(queue[0])
+            else:
+                answer.append(-1)
+        elif order == 'back':
+            if queue:
+                answer.append(queue[-1])
+            else:
+                answer.append(-1)
+
+
+    for a in answer:
+        print(a)
+
+if __name__ == "__main__":
+    solution()
